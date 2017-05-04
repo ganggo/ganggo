a8c82b5dda1670b1f38a305dd8652b6b53d842b2
Lukas Matt <lukas@zauberstuhl.de>
2017-05-04 15:36:54 +0200

    Add workaround for initial lookup

---
+++ app/controllers/people.go
@@ -0,0 +1,34 @@
+package controllers
+//
+// GangGo Application Server
+// Copyright (C) 2017 Lukas Matt <lukas@zauberstuhl.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import (
+  "github.com/revel/revel"
+)
+
+type People struct {
+  *revel.Controller
+}
+
+// XXX not in the federation gem but it seams
+// to be required after a new user is discovered
+// all public posts are fetch from this url
+func (p People) Index() revel.Result {
+  type ToDo struct {}
+  return p.RenderJson(ToDo{})
+}
