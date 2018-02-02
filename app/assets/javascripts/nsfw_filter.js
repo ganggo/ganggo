596ae3de9507557ce315308d9536ada3cd955a70
Lukas Matt <lukas@zauberstuhl.de>
2018-02-02 14:23:44 +0100

    Hide photo entities if nsfw is set

--- app/assets/javascripts/nsfw_filter.js
+++ app/assets/javascripts/nsfw_filter.js
@@ -1,18 +1,22 @@
 // nsfw filter
-$(".card-text[data-markdown]").each(function() {
+$("p[data-markdown]").each(function() {
   var cardText = $(this);
   var cardBody = cardText.closest(".card-body");
   var re = /#nsfw/i;
   var nsfw = re.exec(cardText.text());
+
   if (nsfw !== null) {
     var div = $("<div class='alert alert-danger'>");
     div.append("<i class='fa fa-exclamation'>&nbsp;");
     div.append(msg("javascript.nsfw_warning"));
     cardBody.prepend(div);
+    cardBody.find("a.gallery").hide();
     cardText.hide();
+
     div.click(function() {
+      cardBody.find("a.gallery").show();
       cardText.show();
-      div.hide();
+      div.remove();
     });
   }
 });
